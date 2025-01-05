e46c2d7b99c8f32625767cee1f2ddc87790cf708
Kulshresth Jangid <kulshresthjangid@gmail.com>
2025-01-05 15:26:19 +0530

    adde os

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import csv
 from astrapy import DataAPIClient
 import uuid
+import os
 
 client = DataAPIClient(os.environ.get("API_KEY"))
 db = client.get_database_by_api_endpoint(os.environ.get("DB_URL"))
