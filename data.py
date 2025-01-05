a75082a7b6ff97869236826c37a5d70e74804c10
Kulshresth Jangid <kulshresthjangid@gmail.com>
2025-01-05 15:25:17 +0530

    added the inital script

---
+++ data.py
@@ -0,0 +1,27 @@
+import csv
+from astrapy import DataAPIClient
+import uuid
+
+client = DataAPIClient(os.environ.get("API_KEY"))
+db = client.get_database_by_api_endpoint(os.environ.get("DB_URL"))
+collection = db.get_collection("social_media_engagement")
+csv_file_path = "engagement.csv"
+with open(csv_file_path, mode='r') as file:
+    csv_reader = csv.DictReader(file)
+
+    for row in csv_reader:
+        post_id = str(uuid.uuid4())
+        post_type = row["post_type"]
+        likes = int(row["likes"])
+        shares = int(row["shares"])
+        comments = int(row["comments"])
+
+        collection.insert_one({
+            "post_id": post_id,
+            "post_type": post_type,
+            "likes": likes,
+            "shares": shares,
+            "comments": comments
+        })
+
+print("Data successfully uploaded to AstraDB.")
---
+++ sample.start.sh
@@ -0,0 +1,3 @@
+export API_KEY=
+export DB_URL=
+python3 data.py
